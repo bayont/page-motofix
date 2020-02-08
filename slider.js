20ddf02d2f4518c11f4e6f3d5caaf2282548a2a8
Fabian Fetter <bayontmen@gmail.com>
2020-02-08 23:02:36 +0100

    Added Image Slider

---
+++ slider.js
@@ -0,0 +1,109 @@
+let slid;
+window.addEventListener('load', (event) => {
+     slid = new Slider(".slider");
+  });
+class Slider {
+    constructor(elSelector = ".slider") {
+        this.currentSlide = 0;
+        this.sliderSelector = elSelector;
+        this.elem = null;
+        this.slider = null;
+        this.slides = null;
+        this.prev = null;
+        this.next = null;
+        this.dots = [];
+        
+        this.generateSlider();
+        this.createPrevNext();
+        this.chooseSlide(this.currentSlide);
+        this.createDots();
+    }
+
+    generateSlider() {
+        this.slider = document.querySelector(this.sliderSelector);
+        this.slider.classList.add("slider");
+
+        const slidesContainer = document.createElement("div");
+        slidesContainer.classList.add("container");
+
+        this.slides = this.slider.children;
+        let i = 1;
+        while(this.slides.length) {
+            this.slides[0].classList.add("slide");
+            this.slides[0].style.backgroundImage = `url('${this.slides[0].attributes.src.textContent}')`;
+            slidesContainer.appendChild(this.slides[0]);
+            i++;
+        }
+        this.slides = slidesContainer.querySelectorAll(".slide");
+
+        this.slider.appendChild(slidesContainer);
+
+    }
+
+    createPrevNext() {
+        this.prev = document.createElement("button");
+        this.prev.type = "button";
+        this.prev.classList.add("button");
+        this.prev.addEventListener("click", () => this.prevSlide());
+        
+        this.next = document.createElement("button");
+        this.next.type = "button";
+        this.next.classList.add("button");
+        this.next.addEventListener("click", () => this.nextSlide());
+        
+        let arrow = document.querySelectorAll(".svg-dis.arrow");
+        arrow.forEach((value) => value.classList.remove("svg-dis"));
+        this.prev.appendChild(arrow[0]);
+        this.next.appendChild(arrow[1]);
+
+
+        const nav = document.createElement("div");
+        nav.classList.add("nav");
+        nav.appendChild(this.prev);
+        nav.appendChild(this.next);
+        nav.style.height = `${this.slider.clientHeight} px`;
+        this.slider.appendChild(nav);
+
+    }
+
+    chooseSlide(slide) {
+        console.log("choosing ", slide);
+        this.slides[this.currentSlide].classList.remove("active");
+        this.slides[slide].classList.add("active");
+        this.currentSlide = slide;
+    }
+
+    nextSlide() {
+        this.slides[this.currentSlide].classList.remove("active");
+        if(this.currentSlide >= (this.slides.length - 1))
+            this.currentSlide = 0;
+        else {
+            this.currentSlide++;
+        }
+        this.slides[this.currentSlide].classList.add("active");
+    }
+
+    prevSlide() {
+        this.slides[this.currentSlide].classList.remove("active");
+        if(this.currentSlide <= 0)
+            this.currentSlide = this.slides.length - 1;
+        else {
+            this.currentSlide--;
+        }
+        this.slides[this.currentSlide].classList.add("active");
+    }
+
+    createDots() {
+        const dotsContainer = document.createElement("div");
+        dotsContainer.classList.add("dotsContainer");
+
+       for(let i = 0; i < this.slides.length; i++) {
+            const dot = document.createElement("div"); dot.classList.add("dot");
+            dot.addEventListener("click", () => this.chooseSlide(i));
+            dotsContainer.appendChild(dot);
+            this.dots.push(dot);
+        }
+        this.slider.appendChild(dotsContainer);
+    }
+
+}
