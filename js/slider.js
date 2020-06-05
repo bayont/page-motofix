efb84ee262f9f1c9984ea0e76fbc90326a30b9f7
Fabian Fetter <bayontmen@gmail.com>
2020-06-05 11:40:36 +0200

    5th Meeting the W3 Standards.

--- js/slider.js
+++ js/slider.js
@@ -41,7 +41,7 @@ class Slider {
         let i = 1;
         while(this.slides.length) {
             this.slides[0].classList.add("slide");
-            this.slides[0].style.backgroundImage = `url('${this.slides[0].attributes.itemref.textContent}')`;
+            this.slides[0].style.backgroundImage = `url('${this.slides[0].getAttribute("data-value")}')`;
             slidesContainer.appendChild(this.slides[0]);
             i++;
         }
